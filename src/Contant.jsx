82d75b4cf04e0890a689ba2fb6555453e6592e36
ShanAliUmar <shanumar.dev@gmail.com>
2023-11-04 20:42:50 +0500

    awjei

---
+++ src/Contant.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Contant = () => {
+    return (
+        <>
+            <div className="box12" id='contant'>
+                <img src="" alt="" />
+                <form>
+                    <input type="email" placeholder='Enter Your Email' />
+                    <input type="password" placeholder='Password' />
+                    <input type="submit" placeholder='Write Your Address' />
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default Contant
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import { Api } from './Api'
 import './App.css'
+import Contant from './Contant'
 function Search() {
   const [quer, setQuer] = useState("")
   return (
     <div>
       <div className="app">
         <div className="boxstart">
-        <input type="text" placeholder='Search' className="Search" onChange={e => setQuer(e.target.value)} />
+        <input type="text"  placeholder='Search' className="Search" onChange={e => setQuer(e.target.value)} />
         </div>
         <ul className="list">
           {Api.filter(api => api.name.toLocaleLowerCase().includes(quer)).map(api => ( 
@@ -15,13 +16,14 @@ function Search() {
               <img src={api.img} alt="" className="image" />
               <li key={api.id} className="listItem">{api.name}</li>
               <div className="li">{api.price}$</div>
-              <button className="btn">Click</button>
+              <button className="btn"><a href="#contant">Click</a></button>
             </div>
-
           ))}
         </ul>
       </div>
+      <Contant/>
     </div>
+    
   )
 };
 
