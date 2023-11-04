1a143b480c2d5428506273283b0cdbc2e36a5641
ShanAliUmar <shanumar.dev@gmail.com>
2023-11-04 22:28:43 +0500

    asdh

---
+++ src/Contant.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Contant = () => {
+  return (
+    <div>
+      <div className="box12">
+        <form>
+          <input type="text" placeholder='Enter Your Email'/>
+          <input type="password" placeholder='Password'/>
+          <textarea cols="25" rows="10"></textarea>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Contant
