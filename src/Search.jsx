043ab3040b4eff2d88e839d916d9e8004c989ca0
ShanAliUmar <shanumar.dev@gmail.com>
2023-12-28 09:34:22 +0500

    commit

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,31 +1,37 @@
 import React, { useState } from 'react'
 import { Api } from './Api'
 import './App.css'
-import Contant from './Contant'
 function Search() {
   const [quer, setQuer] = useState("")
+  const [start, setStart] = useState("red")
+  const [showform, setShowform] = useState(false)
+  console.log(showform);
   return (
-    <div>
-      <div className="app">
-        <div className="boxstart">
-        <input type="text" placeholder='Search' className="Search" onChange={e => setQuer(e.target.value)} />
-        </div>
-        <ul className="list">
-          {Api.filter(api => api.name.toLocaleLowerCase().includes(quer)).map(api => ( 
-            <div className="box">
-              <img src={api.img} alt="" className="image" />
-              <li key={api.id} className="listItem">{api.name}</li>
-              <div className="li">{api.price}$</div>
-              <button className="btn">
-                <Contant>
-                <a href='#Contant' >Click</a>
-                </Contant>
-              </button>
-            </div>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <>
+      {
+        showform ? (<div className="form">
+          form data
+          <button onClick={()=> setShowform(false)} >go to back</button>
+        </div>) : (<div className="app">
+          <div className="boxstart">
+            <input type="text" placeholder='Search' className="Search" onChange={e => setQuer(e.target.value)} />
+          </div>
+          <ul className="list">
+            {Api.filter(api => api.name.toLocaleLowerCase().includes(quer)).map(api => (
+              <div className="box">
+                <img src={api.img} alt="" className="image" />
+                <li key={api.id} className="listItem">{api.name}</li>
+                <div className="li">{api.price}$</div>
+                <button className="btn" onClick={(()=> setShowform(!showform))}>
+                  ClicK
+                </button>
+              </div>
+            ))}
+          </ul>
+        </div>)
+      }
+    </>
   )
 };
 export default Search
+
